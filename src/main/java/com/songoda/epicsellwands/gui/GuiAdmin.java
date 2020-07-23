8085b1a244383d50de4358b2cd3c6c5395c294d2
Brianna <bri@songoda.com>
2020-07-23 10:37:06 -0500

    Added the ability to take a wand from the admin menu.

--- src/main/java/com/songoda/epicsellwands/gui/GuiAdmin.java
+++ src/main/java/com/songoda/epicsellwands/gui/GuiAdmin.java
@@ -3,10 +3,15 @@ package com.songoda.epicsellwands.gui;
 import com.songoda.core.compatibility.CompatibleMaterial;
 import com.songoda.core.gui.Gui;
 import com.songoda.core.gui.GuiUtils;
+import com.songoda.core.utils.TextUtils;
 import com.songoda.epicsellwands.EpicSellWands;
 import com.songoda.epicsellwands.wand.Wand;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GuiAdmin extends Gui {
@@ -38,9 +43,24 @@ public class GuiAdmin extends Gui {
 
         for (int i = 0; i < wands.size(); i++) {
             Wand wand = wands.get(i);
-            setButton(i + 9, wand.asItemStack(),
-                    (event) ->
-                            guiManager.showGUI(event.player, new GuiEditWand(plugin, this, wand)));
+
+            ItemStack item = wand.asItemStack();
+
+            ItemMeta meta = item.getItemMeta();
+            List<String> lore = meta.getLore();
+
+            lore.addAll(Arrays.asList("", TextUtils.formatText("&6Left Click &7to edit"), TextUtils.formatText("&6Right Click &7to take")));
+
+            meta.setLore(lore);
+            item.setItemMeta(meta);
+
+            setButton(i + 9, item,
+                    (event) -> {
+                        if (event.clickType == ClickType.LEFT)
+                            guiManager.showGUI(event.player, new GuiEditWand(plugin, this, wand));
+                        else if (event.clickType == ClickType.RIGHT)
+                            event.player.getInventory().addItem(wand.asItemStack());
+                    });
 
         }
     }
